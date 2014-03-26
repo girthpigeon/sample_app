e15c1356c2b5ea0d6433915ab72d25394d44e877
Zack <pajka@wisc.edu>
2014-03-26 16:28:07 -0500

    at page 358

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -121,8 +121,8 @@ describe User do
     end
   end
 
-#  describe "remember token" do
-#    before { @user.save }
-#    its(:remember_token) { should_not be_blank }
-#  end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
