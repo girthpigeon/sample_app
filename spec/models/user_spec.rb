0e69360c172af92a511701d10072271d5e2ad6d9
Zack <pajka@wisc.edu>
2014-04-23 13:42:51 -0500

    Finished exercise 1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,6 +41,27 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
   
+    describe "relationship associations" do
+          let(:other_user) { FactoryGirl.create(:user) }
+          before do
+                @user.save
+                @user.follow!(other_user)
+                other_user.follow!(@user)
+              end
+      
+          it "should destroy associated relationships" do
+                relationships = @user.relationships
+                @user.destroy
+                relationships.present?.should be_false
+              end
+      
+          it "should destroy associated reverse relationships" do
+                reverse_relationships = @user.reverse_relationships
+                @user.destroy
+                reverse_relationships.should be_empty
+              end
+        end
+  
   describe "with admin attribute set to true" do
       before do
           @user.save!
